ae65ef1587e095f962c7801f722ab466eac9a116
Thomas Chartron <thomas.chartron@gmail.com>
2021-11-04 23:10:52 +0100

    update

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let navbar_links = document.querySelector('#nav-links').children;
     let current_location = window.location.href;
     let selected_navbar_link = [...navbar_links].find((item) => {
-        (item.href === current_location)
+        return (item.href === current_location)
     })
     // for (let i = 0; i <= navbar_links.length; i++) {
     //     if (document.querySelector('#nav-links').children)
