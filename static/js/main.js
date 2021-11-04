1398ca16bf2bc569a1a06fd6decc3ae94cbdbb8a
Thomas Chartron <thomas.chartron@gmail.com>
2021-11-04 23:06:34 +0100

    update

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     let navbar_links = document.querySelector('#nav-links').children;
-    [...navbar_links].find((item) => { console.log(item.href.indexOf('/')) })
-    navbar_links.find((item) => {
-        (item.attr(''))
-    });
+    let current_location = window.location.href;
+    let selected_navbar_link = [...navbar_links].find((item) => {
+        (item.href === current_location)
+    })
     // for (let i = 0; i <= navbar_links.length; i++) {
     //     if (document.querySelector('#nav-links').children)
     // }
-    console.log(navbar_links);
+    console.log(selected_navbar_link);
 });
 
