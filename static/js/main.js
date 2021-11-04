44c4bc9ae9f1b86a24adfb6d2d6c43b9327b2a5c
Thomas Chartron <thomas.chartron@gmail.com>
2021-11-04 23:12:16 +0100

    update

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let selected_navbar_link = [...navbar_links].find((item) => {
         return (item.href === current_location)
     })
+    selected_navbar_link.className = "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
     // for (let i = 0; i <= navbar_links.length; i++) {
     //     if (document.querySelector('#nav-links').children)
     // }
