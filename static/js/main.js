94807c56236464b0f21f1b84166c23c7f7e8a9a7
Thomas Chartron <thomas.chartron@gmail.com>
2021-11-04 23:55:27 +0100

    update

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // ---------------- Selected
     let navbar_links = document.querySelector('#nav-links').children;
     let current_location = window.location.href;
     let selected_navbar_link = [...navbar_links].find((item) => {
         return (item.href === current_location)
     })
     selected_navbar_link.className = "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
-    // for (let i = 0; i <= navbar_links.length; i++) {
-    //     if (document.querySelector('#nav-links').children)
-    // }
-    console.log(selected_navbar_link);
 });
 
--- static/js/search.js
+++ static/js/search.js
@@ -3,5 +3,5 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function search() {
-    console.log('search')
+
 }
