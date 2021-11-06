46bb6effeef3f580ea87dbfbacf060a8d76e99d9
Thomas Chartron <thomas.chartron@gmail.com>
2021-11-06 13:17:14 +0100

    update

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+     purge: [
+     './templates/**/*.html',
+     './static/js/**/*.js',
+   ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
