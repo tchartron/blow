e0770b71b2bf9582dc686712b7402574e67f2aa2
Thomas Chartron <thomas.chartron@gmail.com>
2021-11-06 19:41:11 +0100

    update

--- static/js/search.js
+++ static/js/search.js
@@ -1,14 +1,56 @@
 document.addEventListener("DOMContentLoaded", function() {
-  let search_input = document.getElementById('search').addEventListener('click', openSearch)
-  // search();
+  // let search_input = document.getElementById('search').addEventListener('click', openSearch)
+
+  // var openmodal = document.querySelectorAll('.modal-open')
+  // for (var i = 0; i < openmodal.length; i++) {
+  //   openmodal[i].addEventListener('click', function(event){
+  //     event.preventDefault()
+  //     toggleModal()
+  //   })
+  // }
+  let search_input = document.getElementById('search');
+  search_input.addEventListener('click', function(event){
+    event.preventDefault()
+    toggleModal()
+  })
+
+  const overlay = document.querySelector('.modal-overlay')
+  overlay.addEventListener('click', toggleModal)
+
+  var closemodal = document.querySelectorAll('.modal-close')
+  for (var i = 0; i < closemodal.length; i++) {
+    closemodal[i].addEventListener('click', toggleModal)
+  }
+
+  document.onkeydown = function(evt) {
+    evt = evt || window.event
+    var isEscape = false
+    if ("key" in evt) {
+      isEscape = (evt.key === "Escape" || evt.key === "Esc")
+    } else {
+      isEscape = (evt.keyCode === 27)
+    }
+    if (isEscape && document.body.classList.contains('modal-active')) {
+      toggleModal()
+    }
+  };
 });
 
-function openSearch() {
-  console.log("open modal");
-  let search_modal = document.getElementById('search-modal');
-  search_modal.classList.remove('hidden');
-  console.log(search_modal);
+function toggleModal () {
+  const body = document.querySelector('body')
+  // const modal = document.querySelector('.modal')
+  const modal = document.getElementById('search-modal')
+  modal.classList.toggle('opacity-0')
+  modal.classList.toggle('pointer-events-none')
+  body.classList.toggle('modal-active')
 }
+
+// function openSearch() {
+//   console.log("open modal");
+//   let search_modal = document.getElementById('search-modal');
+//   search_modal.classList.remove('hidden');
+//   console.log(search_modal);
+// }
 
 function search() {
   let index = elasticlunr.Index.load(window.searchIndex);
