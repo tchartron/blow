50478b093c4ab9e56989167a4a0c154de1b3ba31
Thomas Chartron <thomas.chartron@gmail.com>
2021-11-06 19:20:43 +0100

    update

--- static/js/search.js
+++ static/js/search.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    search();
+  let search_input = document.getElementById('search').addEventListener('click', openSearch)
+  // search();
 });
 
-function search() {
+function openSearch() {
+  console.log("open modal")
+  let search_modal = document.getElementById('search-modal');
+  console.log(search_modal)
+}
 
+function search() {
+  let index = elasticlunr.Index.load(window.searchIndex);
 }
