0dce86b6b9e83056f51c604c850c894b2ec5dd1e
Thomas Chartron <thomas.chartron@gmail.com>
2021-11-13 14:39:21 +0100

    update

--- src/js/page.js
+++ src/js/page.js
@@ -19,9 +19,9 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log(entry, 'active')
         let res = findCorrespondingTocTitle(entry.target)
         if (typeof res !== 'undefined' && (current_selected_toc === null || current_selected_toc !== res)) {
-          console.log('here')
+          // console.log('here')
             if (current_selected_toc !== null) {
-              console.log(res)
+              // console.log(res)
               current_selected_toc.classList.remove('bg-blue-800');
             }
             current_selected_toc = res
@@ -49,59 +49,10 @@ document.addEventListener("DOMContentLoaded", function() {
   nav_sections_list.forEach((el) => {
     observer.observe(el);
   })
-  // console.log(nav_section_ids)
-
-  // observer.observe(page_content);
-
-  // tocItems.each(function (i) {
-  //   let id = $(this).attr("id").substring(5);
-  //   navSections[i] = document.getElementById(id);
-  // })
-
-  // function isVisible(tocIndex) {
-  //   const current = navSections[tocIndex];
-  //   const next = tocIndex < tocItems.length - 1 ? navSections[tocIndex + 1] : $("section.section").get(1);
-
-  //   const c = current.getBoundingClientRect();
-  //   const n = next.getBoundingClientRect();
-  //   const h = (window.innerHeight || document.documentElement.clientHeight);
-
-  //   return (c.top <= h) && (c.top + (n.top - c.top) - menuBarHeight >= 0);
-  // }
-
-  // function activateIfVisible() {
-  //   for (b = true, i = 0; i < tocItems.length; i++) {
-  //     if (b && isVisible(i)) {
-  //       tocItems[i].classList.add('is-active');
-  //       b = false;
-  //     } else
-  //       tocItems[i].classList.remove('is-active');
-  //   }
-  // }
-
-  // var isTicking = null;
-  // window.addEventListener('scroll', () => {
-    // if (!isTicking) {
-      // window.requestAnimationFrame(() => {
-        // activateIfVisible();
-      //   isTicking = false;
-      // });
-      // isTicking = true;
-    // }
-  // }, false);
 });
-
-function activeTocItem() {
-  let scroll_position = window.scrollY
-  console.log(scroll_position)
-}
 
 function findCorrespondingTocTitle(section) {
   return [...document.querySelectorAll('#toc li a')].find((item) => {
-    // console.log(item)
-    // console.log(`#${section.id}`)
-    // console.log(item.href.substring(item.href.indexOf("#")))
     return item.href.substring(item.href.indexOf("#")) === `#${section.id}`
   })
-  // return result
 }
