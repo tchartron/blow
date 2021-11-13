9baf49d870787f42aa5fbaed07a9c97408164995
Thomas Chartron <thomas.chartron@gmail.com>
2021-11-13 14:13:41 +0100

    update

--- src/js/page.js
+++ src/js/page.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   // ---------------- TOC Scrollspy --------------------
   const navbar_height = document.getElementById('navbar').clientHeight
   const table_of_content = document.getElementById('toc')
+  const table_of_content_items = document.querySelectorAll('#toc li a')
   const page_content = document.getElementById('page-content')
   // const navSections = new Array($('.toc').length);
   window.addEventListener('scroll', activeTocItem)
@@ -11,7 +12,9 @@ document.addEventListener("DOMContentLoaded", function() {
       // Add 'active' class if observation target is inside viewport
       if (entry.intersectionRatio > 0) {
         console.log(entry, 'active')
-        entry.target.classList.add('bg-blue-800');
+        let res = findCorrespondingTocTitle(entry.target)
+        console.log(res)
+        res.classList.add('bg-blue-800');
       } else {
         console.log(entry, 'inactive')
         entry.target.classList.remove('bg-blue-800');
@@ -23,15 +26,15 @@ document.addEventListener("DOMContentLoaded", function() {
   })
 
   // const boxElList = document.querySelectorAll('.box');
-  var nav_sections = [];
-  [...document.querySelectorAll('#toc li a')].forEach((item)=> {
-    nav_sections.push(item.href.substring(item.href.indexOf("#")))
+  var nav_section_ids = [];
+  [...table_of_content_items].forEach((item)=> {
+    nav_section_ids.push(item.href.substring(item.href.indexOf("#")))
   })
-  const list = document.querySelectorAll(nav_sections.join(','));
-  list.forEach((el) => {
+  const nav_sections_list = document.querySelectorAll(nav_section_ids.join(','));
+  nav_sections_list.forEach((el) => {
     observer.observe(el);
   })
-  // console.log(nav_sections)
+  // console.log(nav_section_ids)
 
   // observer.observe(page_content);
 
@@ -76,4 +79,14 @@ document.addEventListener("DOMContentLoaded", function() {
 function activeTocItem() {
   let scroll_position = window.scrollY
   console.log(scroll_position)
+}
+
+function findCorrespondingTocTitle(section) {
+  let result = [...document.querySelectorAll('#toc li a')].find((item) => {
+    console.log(item)
+    console.log(`#${section.id}`)
+    console.log(item.href.substring(item.href.indexOf("#")))
+    return item.href.substring(item.href.indexOf("#")) === `#${section.id}`
+  })
+  return result
 }
