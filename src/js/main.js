86063e23aec32e3761e8dcb786705efe0920d938
Thomas Chartron <thomas.chartron@gmail.com>
2021-11-14 15:23:23 +0100

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   // ---------------- Selected Navbar Link -------------------------
-  let navbar_links = document.querySelector('.nav-links').children;
+  let navbar_links = document.querySelectorAll('.nav-links a');
   let current_location = window.location.href;
   let selected_navbar_link = [...navbar_links].filter((item) => {
     return (item.href === current_location)
