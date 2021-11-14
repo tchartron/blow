f18bb231063a378959b9cc6b1f47d650b1b02714
Thomas Chartron <thomas.chartron@gmail.com>
2021-11-14 20:15:27 +0100

    update

--- src/js/main.js
+++ src/js/main.js
@@ -66,10 +66,12 @@ function toggleMobileMenu() {
   let menu = document.querySelector('#mobile-menu div.nav-links');
   if ([...menu.classList].includes('h-screen')) {
     document.body.classList.remove("overflow-hidden", "relative")
+    document.documentElement.classList.remove("overscroll-none",)
     menu.classList.remove('h-screen')
     menu.classList.add('h-0')
   } else {
     document.body.classList.add("overflow-hidden", "relative")
+    document.documentElement.classList.add("overscroll-none",)
     menu.classList.remove('h-0')
     menu.classList.add('h-screen')
   }
